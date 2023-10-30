cf8f55d1c425260cf9c8393f7161b2864e34417c
sardorus <shukhratovsardor@gmail.com>
2023-10-29 21:39:52 -0500

    first commit

---
+++ src/test/java/com/sardorus/basicSpringProject/BasicSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sardorus.basicSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
