8df1f024606f4df4f2ecd38dc59de722d9ed9f2b
sardorus <shukhratovsardor@gmail.com>
2023-12-05 21:55:47 -0600

    configured azure sql db yml file

--- src/main/java/com/sardorus/basicSpringProject/student/StudentConfig.java
+++ src/main/java/com/sardorus/basicSpringProject/student/StudentConfig.java
@@ -11,18 +11,18 @@ import java.util.List;
 @Configuration
 public class StudentConfig {
 
-    @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository){
-        return args -> {
-            Student marian = new Student("Mariam","mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5));
-            Student alex = new Student("Alex","alex@gmail.com", LocalDate.of(2004, Month.JANUARY, 5));
-
-            repository.saveAll(
-                    List.of(marian, alex)
-            );
-
-            repository.saveAll(List.of(marian, alex));
-
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository repository){
+//        return args -> {
+//            Student marian = new Student("Mariam","mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5));
+//            Student alex = new Student("Alex","alex@gmail.com", LocalDate.of(2004, Month.JANUARY, 5));
+//
+//            repository.saveAll(
+//                    List.of(marian, alex)
+//            );
+//
+//            repository.saveAll(List.of(marian, alex));
+//
+//        };
+//    }
 }
